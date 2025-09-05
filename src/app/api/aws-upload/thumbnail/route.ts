8f4cf49878413b76b975c753dbbe72162fdff476
Bartion1 <u59_bardeu_waw@technischools.com>
2025-09-05 18:43:14 +0200

    feat(thumbnail upload): implemented the validation in the route

---
+++ src/app/api/aws-upload/thumbnail/route.ts
@@ -0,0 +1,73 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { currentUser } from '@clerk/nextjs/server';
+
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { s3Client, BUCKET_NAME, S3_FOLDERS } from '@/lib/s3';
+
+import z from 'zod';
+import { uploadThumbnailRequestSchema } from '@/types/upload/thumbnail.types';
+
+export async function POST(request: NextRequest) {
+  try {
+    const user = await currentUser();
+    
+    if (!user || !user.id) {
+      return NextResponse.json(
+        { error: 'Unauthorized' }, 
+        { status: 401 }
+      );
+    }
+
+    const body = await request.json();
+    const { videoId, fileName, fileType, fileSize } = body;
+
+    const validatedData = uploadThumbnailRequestSchema.parse({ fileName, fileType, fileSize });
+
+    const fileExtension = validatedData.fileName.split('.').pop();
+    const thumbnailFileName = `${videoId}.${fileExtension}`;
+    const s3Key = `${S3_FOLDERS.THUMBNAILS}${thumbnailFileName}`;
+
+    const putObjectCommand = new PutObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: s3Key,
+      ContentType: validatedData.fileType,
+      ContentLength: validatedData.fileSize,
+      Metadata: {
+        'uploaded-by': user.id,
+        'video-id': videoId,
+        'original-name': validatedData.fileName
+      }
+    });
+
+    const uploadUrl = await getSignedUrl(s3Client, putObjectCommand, {
+      expiresIn: 600
+    });
+
+    const thumbnailUrl = `https://${BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${s3Key}`;
+
+    return NextResponse.json({
+       uploadUrl,
+       thumbnailUrl,
+       s3Key ,
+       expiresIn: 600
+    });
+
+  } catch (error) {
+      if (error instanceof z.ZodError) {
+        return NextResponse.json(
+          { error: error.issues[0].message },
+          { status: 400 }
+        );
+      }
+  
+      console.error('Presigned URL generation for thumbnail failed:', error);
+      return NextResponse.json(
+        { 
+          error: 'Failed to generate upload URL',
+          details: error instanceof Error ? error.message : 'Unknown error'
+        }, 
+        { status: 500 }
+      );
+    }
+  }
