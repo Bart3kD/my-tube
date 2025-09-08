84a537ed9ca3929b69d2af5a884f171584fbc820
Bart3kD <114722169+Bart3kD@users.noreply.github.com>
2025-09-08 10:57:48 +0200

    refactor(layout): removed depricated property from UserButton

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -42,7 +42,6 @@ export default function Layout({ children }: LayoutProps) {
               </SignedOut>
               <SignedIn>
                 <UserButton 
-                  afterSignOutUrl="/" 
                   appearance={{
                     elements: {
                       avatarBox: "w-8 h-8"
